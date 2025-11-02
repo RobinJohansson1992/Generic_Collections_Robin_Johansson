8701441506426a3d60cf1ffe71edde34136eae04
RobinJohansson1992 <robin-johansson@live.com>
2025-11-02 19:39:24 +0100

    Add project files.

---
+++ Generic_Collections_Robin_Johansson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generic_Collections_Robin_Johansson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
